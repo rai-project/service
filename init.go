b9e5d93ed9c9f6cfabd24fcfbcb14d298f7e6c9b
abhiutd <abhibits2012@gmail.com>
2018-08-03 15:25:10 -0500

    upgrade to dep v0.5

--- init.go
+++ init.go
@@ -3,8 +3,8 @@ package service
 import (
 	"github.com/rai-project/config"
 	"github.com/rai-project/logger"
-	"github.com/rai-project/tracer"
-	"github.com/rai-project/tracer/zipkin"
+	//"github.com/rai-project/tracer"
+	//"github.com/rai-project/tracer/zipkin"
 	"github.com/sirupsen/logrus"
 )
 
